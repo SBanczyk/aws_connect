f0058bcf82a0a6d34a4e27a754820f8da79c92da
Szymon Bańczyk <sbanczyk02@interia.pl>
2023-11-27 17:43:58 +0100

    Implemented FastAPI with handling exceptions. (#4)
    * Implemented FastAPI with handling incorrect `credentials` and incorrect `instance_id` exceptions.
    
    * Updated requirements.
    
    * Resolved commented problems and synced dependencies with requirements.txt.
    
    * Update aws_connect.py

--- aws_connect.py
+++ aws_connect.py
@@ -1,45 +1,55 @@
-import sys
 import boto3
 import configparser
 import botocore.exceptions
+from fastapi import FastAPI, HTTPException
 
 
-def aws_connect(config_file_path: str, action: str):
-    config = configparser.ConfigParser()
-    config.read(config_file_path)
+app = FastAPI()
+config = configparser.ConfigParser()
+config.read("config.ini")
+try:
+    session = boto3.Session(
+        aws_access_key_id=config['CREDENTIALS']['aws_access_key_id'],
+        aws_secret_access_key=config['CREDENTIALS']['aws_secret_access_key'],
+        region_name=config['CREDENTIALS']['region_name']
+    )
+    ec2 = session.resource('ec2')
+except KeyError:
+    print(f"config.ini does not exist, is of incorrect type, has incorrect structure or contains "
+          f"incorrect credential names")
+
+
+@app.get("/instances/{instance_id}/status")
+async def status(instance_id: str):
     try:
-        session = boto3.Session(
-            aws_access_key_id=config['CREDENTIALS']['aws_access_key_id'],
-            aws_secret_access_key=config['CREDENTIALS']['aws_secret_access_key'],
-            region_name=config['CREDENTIALS']['region_name']
-        )
-        ec2 = session.resource('ec2')
-        instance = ec2.Instance(config['CREDENTIALS']['instance_id'])
-    except KeyError:
-        print(f"{config_file_path} does not exist, is of incorrect type, has incorrect structure or contains "
-              f"incorrect credential names")
-        return
-    try:
-        if action == "start":
-            instance.start()
-        elif action == "stop":
-            instance.stop()
-        elif action == "state":
-            print(instance.state)
-        else:
-            print("Available actions: start, stop, state")
+        instance = ec2.Instance(instance_id)
+        return instance.state
     except botocore.exceptions.ClientError as err:
-        print(err)
-        return
+        if "InvalidInstanceID" in err.response["Error"]["Code"]:
+            raise HTTPException(status_code=404, detail="Invalid instance id")
+        else:
+            raise err
 
 
-def main():
-    if len(sys.argv) != 3:
-        print("Usage: ./aws_connect <config_file_path> <action>")
-    else:
-        aws_connect(sys.argv[1], sys.argv[2])
-    return 0
+@app.post("/instances/{instance_id}/start", status_code=204)
+async def start(instance_id: str):
+    try:
+        instance = ec2.Instance(instance_id)
+        instance.start()
+    except botocore.exceptions.ClientError as err:
+        if "InvalidInstanceID" in err.response["Error"]["Code"]:
+            raise HTTPException(status_code=404, detail="Invalid instance id")
+        else:
+            raise err
 
 
-if __name__ == '__main__':
-    sys.exit(main())
+@app.post("/instances/{instance_id}/stop", status_code=204)
+async def stop(instance_id: str):
+    try:
+        instance = ec2.Instance(instance_id)
+        instance.stop()
+    except botocore.exceptions.ClientError as err:
+        if "InvalidInstanceID" in err.response["Error"]["Code"]:
+            raise HTTPException(status_code=404, detail="Invalid instance id")
+        else:
+            raise err
